ef24e3ec629715ade41ac22f8a71478830c7cdf1
WillianXG <wilian.mine789@gmail.com>
2023-07-09 20:37:56 -0300

    ajuste na logo navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,7 +4,7 @@ import styles from './navbar.module.css';
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}><h3>Con3kta</h3></div>
+      <div className={styles.logo}><h3><span className={styles.logo_simbol}>&lt;/&gt;</span>Con3kta</h3></div>
       <button className={styles.button}>Login</button>
     </div>
   );
